6d4409f3c5f58cf2623f61744bb2fc39267f7097
unknown <michnom179@gmail.com>
2021-09-27 23:17:26 +0200

    planning new components

---
+++ src/routes/createSchedule/eventsManagement/eventsMenu/EventsMenu.tsx
@@ -0,0 +1,52 @@
+import EventsMenuCSS from './eventsMenu.module.scss';
+
+export const EventsMenu = () => {
+    return(
+        <div className={EventsMenuCSS.menuContainer}>
+            <div className={EventsMenuCSS.menuContainer__event}>
+                <div className={EventsMenuCSS.menuContainer__event__description}>Add new event</div>
+                <div className={EventsMenuCSS.menuContainer__event__add}>+</div>
+            </div>
+            <div className={EventsMenuCSS.menuContainer__event}>
+                <div className={EventsMenuCSS.menuContainer__event__description}>Add new event</div>
+                <div className={EventsMenuCSS.menuContainer__event__actions}>R</div>
+                <div className={EventsMenuCSS.menuContainer__event__actions}>M</div>
+            </div>
+            <div className={EventsMenuCSS.menuContainer__event}>
+                <div className={EventsMenuCSS.menuContainer__event__description}>Add new event</div>
+                <div className={EventsMenuCSS.menuContainer__event__actions}>R</div>
+                <div className={EventsMenuCSS.menuContainer__event__actions}>M</div>
+            </div>
+            <div className={EventsMenuCSS.menuContainer__event}>
+                <div className={EventsMenuCSS.menuContainer__event__description}>Add new event</div>
+                <div className={EventsMenuCSS.menuContainer__event__actions}>R</div>
+                <div className={EventsMenuCSS.menuContainer__event__actions}>M</div>
+            </div>
+            <div className={EventsMenuCSS.menuContainer__event}>
+                <div className={EventsMenuCSS.menuContainer__event__description}>Add new event</div>
+                <div className={EventsMenuCSS.menuContainer__event__actions}>R</div>
+                <div className={EventsMenuCSS.menuContainer__event__actions}>M</div>
+            </div>
+            <div className={EventsMenuCSS.menuContainer__event}>
+                <div className={EventsMenuCSS.menuContainer__event__description}>Add new event</div>
+                <div className={EventsMenuCSS.menuContainer__event__actions}>R</div>
+                <div className={EventsMenuCSS.menuContainer__event__actions}>M</div>
+            </div>
+            <div className={EventsMenuCSS.menuContainer__event}>
+                <div className={EventsMenuCSS.menuContainer__event__description}>Add new event</div>
+                <div className={EventsMenuCSS.menuContainer__event__actions}>R</div>
+                <div className={EventsMenuCSS.menuContainer__event__actions}>M</div>
+            </div>
+            <div className={EventsMenuCSS.menuContainer__event}>
+                <div className={EventsMenuCSS.menuContainer__event__description}>Add new event</div>
+                <div className={EventsMenuCSS.menuContainer__event__actions}>R</div>
+                <div className={EventsMenuCSS.menuContainer__event__actions}>M</div>
+            </div>
+            <div className={EventsMenuCSS.menuContainer__event}>
+                <div className={EventsMenuCSS.menuContainer__event__description}>Add new event</div>
+                <div className={EventsMenuCSS.menuContainer__event__actions}>R</div>
+                <div className={EventsMenuCSS.menuContainer__event__actions}>M</div>
+            </div>
+        </div>
+    )
+}
--- src/routes/createSchedule/scheduleManagement/ScheduleManagement.tsx
+++ src/routes/createSchedule/scheduleManagement/ScheduleManagement.tsx
@@ -1,11 +1,13 @@
 import ScheduleManagementCSS from './scheduleManagement.module.scss';
 //components
 import { SmallCalendar } from '../smallCalendar/SmallCalendar';
+import { EventsMenu } from '../eventsManagement/eventsMenu/EventsMenu';
 
 export const ScheduleManagement = () => {
     return(
         <div className={ScheduleManagementCSS.scheduleManagementContainer}>
             <SmallCalendar/>
+            <EventsMenu/>
         </div>
     )
 }
