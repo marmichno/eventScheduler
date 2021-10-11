faa8e5f2dcfe3f56d1ade59453d1e395dd1413cc
unknown <michnom179@gmail.com>
2021-10-11 23:25:31 +0200

    login route

--- src/routes/login/Login.tsx
+++ src/routes/login/Login.tsx
@@ -1,15 +1,32 @@
 import LoginCSS from './login.module.scss';
+//components
+import { Navbar } from '../../components/navbar/Navbar';
+import { LoginInputField } from './loginInputField/LoginInputField';
+//formik
+import {Formik, Form, Field, ErrorMessage} from 'formik';
 
 export const Login = () => {
     return(
         <div className={LoginCSS.mainContainer}>
+            <Navbar/>
             <div className={LoginCSS.mainContainer__contentContainer}>
-                <div className={LoginCSS.mainContainer__contentContainer__descriptionContainer}>
+                <Formik initialValues={{
+                    login: '',
+                    password: ''
+                }}
+                // validationSchema={validationSchema}
+                onSubmit={ data =>{
+                    console.log(data);
+                }}
+                >
+                    <Form className={LoginCSS.mainContainer__contentContainer__formContainer}>
+                        <h2>Login</h2>
+                        <LoginInputField label="Login" name="login" type="text"/>
+                        <LoginInputField label="Password" name="password" type="password"/>
+                        <button type="submit">Login</button>
+                    </Form>
 
-                </div>
-                <div className={LoginCSS.mainContainer__contentContainer__formContainer}>
-                    
-                <div>
+                </Formik>
             </div>
         </div>
     )
