8d85b458bc689cdaffdd285d8d015d78f08f1b20
marmichno <michnom179@gmail.com>
2022-01-09 21:42:25 +0100

    testing diffrent apis for map

--- src/routes/eventsFinder/displayEvents/eventsMap/EventsMapContainer.tsx
+++ src/routes/eventsFinder/displayEvents/eventsMap/EventsMapContainer.tsx
@@ -1,7 +1,20 @@
 import EventsMapCSS from './eventsMapContainer.module.scss';
+//leaflet
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
 
 export const EventsMapContainer = () => {
-    return(
-        <div className={EventsMapCSS.mainContainer}>events map</div>
+
+    return (
+        <MapContainer
+            center={[52.653160, 19.059940]}
+            zoom={7}
+            style={{ width: '50%', height: '100%' }}
+            scrollWheelZoom={true}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+        </MapContainer>
     )
 }
